f1e92dc269d84a4c9e1023a7d3fcd544811e2d91
Seraphinsupinfo <93323611+Seraphinsupinfo@users.noreply.github.com>
2023-01-20 10:26:51 +0100

    whitelist

--- src/WhiteListsEmails.java
+++ src/WhiteListsEmails.java
@@ -3,14 +3,26 @@ import java.util.*;
 
 public class WhiteListsEmails {
     private static ArrayList<String> whiteList = new ArrayList<String>();
+    private static String email;
 
     public WhiteListsEmails(Optional<Connection> connection) throws SQLException {
         //Le constructeur qui charge toutes les valeurs de la table des adresses mails dans la variable grace àla fonction returnWhiteList
         whiteList = returnWhitelist(connection);
+        String email = "";
     }
 
     public static ArrayList<String> getWhiteList() {
         return whiteList;
+    }
+
+
+
+    private void insertWhitelist (Optional<Connection> connection) throws SQLException {
+        String sql = "INSERT INTO Annuaire (email) VALUES(?)";
+        PreparedStatement statement = connection.get().prepareStatement(sql);
+        //en spécifiant bien les types SQL cibles
+        statement.setString(1, email);
+        statement.executeUpdate();
     }
 
     private ArrayList<String> returnWhitelist(Optional<Connection> connection) throws SQLException {
@@ -20,7 +32,6 @@ public class WhiteListsEmails {
             try (Statement st = connection.get().createStatement()){
                 try (ResultSet rs = st.executeQuery("SELECT email FROM white_list")){
                     while (rs.next()) {
-                        System.out.println(rs.getString("email"));
                         whitelist.add(rs.getString("email"));
                     }
                 }
