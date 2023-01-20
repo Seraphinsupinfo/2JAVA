4b52b666c847fd2f06e7d8fd9cb051e443557418
Seraphinsupinfo <93323611+Seraphinsupinfo@users.noreply.github.com>
2023-01-20 11:26:22 +0100

    ajout pseudo

--- src/Users.java
+++ src/Users.java
@@ -2,6 +2,7 @@ public class Users {
     private int ID;
     private String firstName;
     private String lastName;
+    private String pseudo;
     private String email;
     private String role;
     private String password;
