312241611264831dc831c710333f13bcab15c926
ryandord <84190123+ryandord@users.noreply.github.com>
2023-01-25 11:35:23 +0100

    liaison page stock

--- src/HomeSeller.java
+++ src/HomeSeller.java
@@ -1,4 +1,7 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Objects;
 
 public class HomeSeller {
     public JPanel panelMain;
@@ -13,5 +16,13 @@ public class HomeSeller {
 
     HomeSeller(){
         labelText.setText("Bonjour "+ firstName + ", vous êtes assignés au magasin " + shop);
+
+        voirLesStocksButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Display.stockSeller();
+            }
+        });
     }
 }
+
