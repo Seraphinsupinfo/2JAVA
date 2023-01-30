2f33a532fe4dae9edf4493e05924c80f36bc63f6
ryandord <84190123+ryandord@users.noreply.github.com>
2023-01-30 16:03:12 +0100

    taille de popup dynamiquye

--- src/Display.java
+++ src/Display.java
@@ -4,78 +4,78 @@ public class Display {
     public static JFrame frame;
     public static JFrame Errframe;
     public static void main(){
-        //Initialisation menu
+        //Initialisation page se connecter/s'inscrire et initialisation de la fenetre
         Display.frame = new JFrame("Istore");
         frame.setContentPane(new DisplayMain().panelMain);
         frame.setSize(300,220);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
+        Display.errorPopUp("cbzcbeuz,regergerg;greg regergregre gregreger gregregerg regregerreg");
+        Display.errorPopUp("cbzcbeuz");
+        Display.errorPopUp("cbzcb regregerg regregerreg");
     }
 
     protected static void signUp(){
-        //Initialisation menu
+        //Initialisation page s'inscrire
         frame.setContentPane(new SignUp().panelMain);
         frame.setSize(400,300);
     }
     protected static void logIn(){
-        //Initialisation menu
+        //Initialisation page se connecter
         frame.setContentPane(new Login().panelMain);
         frame.setSize(350,250);
     }
 
-    protected static void test(){
-        //Initialisation menu
-      stockSeller();
-    }
-
     protected static void homeAdmin(){
-        //Initialisation menu
+        //Initialisation accueil admin
         frame.setContentPane(new HomeAdmin().panelMain);
         frame.setSize(600,500);
     }
     protected static void homeSeller(){
-        //Initialisation menu
+        //Initialisation accueil vendeur
         frame.setContentPane(new HomeSeller().panelMain);
         frame.setSize(600,500);
     }
     protected static void homeUser(){
-        //Initialisation menu
+        //Initialisation accueil personne lambda
         frame.setContentPane(new HomeUser().panelMain);
         frame.setSize(600,350);
     }
     protected static void stockSeller(){
-        //Initialisation menu
+        //Initialisation page stock vendeur
         frame.setContentPane(new StockSeller().panelMain);
         frame.setSize(1000,750);
     }
 
     protected static void whiteList(){
-        //Initialisation menu
+        //Initialisation page gestion des whitelist
         frame.setContentPane(new WhiteListGestion().panelMain);
         frame.setSize(1000,750);
     }
 
     protected static void userManagement(){
-        //Initialisation menu
+        //Initialisation gestion des utilisateurs
         frame.setContentPane(new UserManagement().panelMain);
         frame.setSize(1000,750);
     }
     protected static void stockAdmin(){
-        //Initialisation menu
+        //Initialisation gestion des stock coté admin
         frame.setContentPane(new StockAdmin().panelMain);
         frame.setSize(1000,750);
     }
     protected static void shopManagement(){
-        //Initialisation menu
+        //Initialisation gestion des magasins
         frame.setContentPane(new ShopManagement().panelMain);
         frame.setSize(1000,750);
     }
     public static void errorPopUp(String message){
-        //Initialisation menu
+
+        //Initialisation de la pop up d'erreur avec un message
+        int w =message.length();
         ErrorPopUp.varText=message;
         Display.Errframe = new JFrame("Erreur");
         Errframe.setContentPane(new ErrorPopUp().panelMain);
-        Errframe.setSize(220,180);
+        Errframe.setSize(220+(w * 4),180);
         Errframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Errframe.setVisible(true);
     }
